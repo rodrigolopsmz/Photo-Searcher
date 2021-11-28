9a76d411fe31405e308317218cc2415474cf29b1
rlopezsmz <rodrigo.lop.sm@gmail.com>
2021-11-28 19:24:21 -0300

    Added some changes for a better visualization

--- client/src/components/Busqueda/subcomponents/Panel/subcomponents/Cards/Card.js
+++ client/src/components/Busqueda/subcomponents/Panel/subcomponents/Cards/Card.js
@@ -6,7 +6,7 @@ export function Card (props){
     return(
         <div className={styles.div}>
             <h1 className={styles.name}>Title: {props.name.charAt(0).toUpperCase() +props.name.slice(1)} </h1>
-            <img className={styles.img} alt={'SearchedImage'} src={props.img}/>
+            <img className={styles.img} alt={'SearchedImage'} src={props.img} style={{height: '250px'}}/>
         </div>
     )
 
